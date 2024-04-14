cf062dbc7f81cfc7a9bda8bdb2f537cc65b764e5
Diogo Andrade <diogoa.benedito@outlook.com>
2024-04-14 15:50:23 -0300

    Set Winner
    Criado testes para escolher vencedor.
    Alteração do struct de apostas.

--- contracts/Betting.sol
+++ contracts/Betting.sol
@@ -9,18 +9,27 @@ contract Betting {
     address public owner;
 
     // Estrutura para representar um desafio
-    struct Bets {
+    struct Bet {
         address challenged;
         address challenger;
         address judge;
         uint256 betAmount;
+        uint256 betBalance;
         bool active;
         bool accepted;
         address winner;
+        uint256 createdAt;
+        uint256 acceptedAt;
+        uint256 finishedAt;
+    }
+
+    struct BetResponse {
+        Bet[] bets;
+        uint  total;
     }
 
     // Mapeia o endereço do desafiado para os desafios criados por ele
-    Bets[] public bets;
+    Bet[] public bets;
 
     // Evento para notificar quando um desafio é criado
     event ChallengeCreated(address indexed challenged, uint256 betAmount);
@@ -31,10 +40,13 @@ contract Betting {
     // Evento para notificar quando um juiz é atribuído à um desafio
     event JudgeDefined(address indexed judge);
 
+    // Evento para notificar vencedor do desafio
+    event ChallengeWinner(address indexed winner, uint256 indexed reward);
+
     modifier validJudge(address _judge, uint _challenge) {
         require(_judge != bets[_challenge].challenged, "O desafiado nao pode ser o juiz.");
         require(_judge != bets[_challenge].challenger, "O desafiante nao pode ser o juiz.");
-        require(bets[_challenge].judge == address(0), "Ja existe um juiz para esse desafio");
+        require(bets[_challenge].judge == address(0), "Ja existe um juiz para esse desafio.");
         _;
     }
 
@@ -47,14 +59,18 @@ contract Betting {
         address _challenged = msg.sender;
 
         // Cria o desafio
-        Bets memory newChallenge = Bets({
+        Bet memory newChallenge = Bet({
             challenged: _challenged,
             challenger: address(0),
             judge: address(0),
             betAmount: msg.value,
+            betBalance: msg.value,
             active: true,
             accepted: false,
-            winner: address(0)
+            winner: address(0),
+            createdAt: block.timestamp,
+            acceptedAt: 0,
+            finishedAt: 0
         });
 
         // Adiciona o desafio à lista do desafiado
@@ -67,7 +83,7 @@ contract Betting {
     // Função para aceitar um desafio como desafiante
     function acceptChallenge(uint256 _challengeId) external payable {
         // Obtém o desafio da lista de desafios
-        Bets storage challenge = bets[_challengeId];
+        Bet storage challenge = bets[_challengeId];
 
         // Garante que o desafio esteja ativo e ainda não tenha sido aceito
         require(challenge.active && !challenge.accepted, "Desafio nao esta ativo ou ja foi aceito.");
@@ -80,7 +96,9 @@ contract Betting {
 
         // Define o desafiante como o chamador da função e marca o desafio como aceito
         challenge.challenger = msg.sender;
+        challenge.betBalance += msg.value;
         challenge.accepted = true;
+        challenge.acceptedAt = block.timestamp;
 
         emit ChallengeAccepted(challenge.challenger, _challengeId);
     }
@@ -88,7 +106,7 @@ contract Betting {
     function setJudge(uint _challengeId) external validJudge(msg.sender, _challengeId) {
         require(bets[_challengeId].active, "O desafio precisa estar ativo.");
 
-        Bets storage desafio = bets[_challengeId];
+        Bet storage desafio = bets[_challengeId];
         desafio.judge = msg.sender;
 
         emit JudgeDefined(desafio.judge);
@@ -96,11 +114,14 @@ contract Betting {
 
     // Função para o juiz declarar o vencedor
     function setWinner(uint256 _challengeId, address _winner) external payable {
-        // Somente o juiz pode chamar esta função
-        require(msg.sender == bets[_challengeId].judge, "Somente o juiz pode declarar o vencedor");
-
         // Obtém o desafio da lista do desafiado
-        Bets storage desafio = bets[_challengeId];
+        Bet storage desafio = bets[_challengeId];
+
+        // Somente o juiz pode chamar esta função
+        require(msg.sender == desafio.judge, "Somente o juiz pode declarar o vencedor.");
+
+        // O vencedor deve ser o challanger ou o challenged
+        require(_winner == desafio.challenger || _winner == desafio.challenged, "O vencedor deve ser o desafiado ou o desafiante.");
 
         // Garante que o desafio esteja ativo e tenha sido aceito
         require(desafio.active && desafio.accepted, "Desafio nao esta ativo ou nao foi aceito");
@@ -108,20 +129,85 @@ contract Betting {
         // Define o vencedor
         desafio.winner = _winner;
         desafio.active = false;
+        desafio.finishedAt = block.timestamp;
 
         // Calcula os pagamentos
-        uint256 valorPremio = (desafio.betAmount * 8) / 10; // 80% para o vencedor
-        uint256 taxaAdministrativa = (desafio.betAmount * 1) / 10; // 10% para fins administrativos
-        uint256 pagamentoJuiz = (desafio.betAmount * 1) / 10; // 10% para o juiz
+        uint256 valorPremio = (desafio.betBalance * 8) / 10; // 80% para o vencedor
+        uint256 taxaAdministrativa = desafio.betBalance / 10; // 10% para fins administrativos
+        uint256 pagamentoJuiz = desafio.betBalance / 10; // 10% para o juiz
+
+        require (pagamentoJuiz + valorPremio + taxaAdministrativa == desafio.betBalance, "Falha nos calculos dos premios, fale com um administrador.");
 
         // Transfere os pagamentos
         payable(_winner).transfer(valorPremio);
         payable(desafio.judge).transfer(pagamentoJuiz);
         payable(owner).transfer(taxaAdministrativa);
+
+        emit ChallengeWinner(_winner, valorPremio);
     }
 
-    function getDesafios() public view returns (Bets[] memory) {
+    function getDesafios() public view returns (Bet[] memory) {
         return bets;
+    }
+    
+    function getBetsResponse() public view returns (BetResponse memory) {
+        Bet[] memory _bets = new Bet[](20);
+        uint _last;
+
+        if (bets.length >= 20) {
+            _last = 20;
+        } else {
+            _last = bets.length;
+        }
+
+        for (uint index = 0; index < _last; index++) {
+            _bets[index] = bets[index];
+        }
+
+        return BetResponse({
+            bets: _bets,
+            total: bets.length
+        });
+    }
+
+    function getBetsResponse(uint start) public view returns (BetResponse memory) {
+        require(bets.length < start, unicode"Não há desafios suficientes.");
+
+        Bet[] memory _bets = new Bet[](20);
+        uint _last;
+
+        if (bets.length + start > 20) {
+            _last = 20;
+        } else {
+            _last = bets.length;
+        }
+
+        for (uint index = start; index < _last; index++) {
+            _bets[index] = bets[index];
+        }
+
+        return BetResponse({
+            bets: _bets,
+            total: bets.length
+        });
+    }
+
+    function getBetsByAddress(address wallet) public view returns (BetResponse memory) {
+        uint256 betIndex = 0;
+
+        Bet[] memory _betsByAddress;
+
+        for (uint i = 0; i < bets.length; i++) {
+            if (bets[i].challenger == wallet || bets[i].challenged == wallet) {
+                _betsByAddress[betIndex] = (bets[i]);
+                betIndex++;
+            }
+        }
+
+        return BetResponse({
+            bets: _betsByAddress,
+            total: _betsByAddress.length + 1
+        });
     }
 
     function balance() public view returns (uint) {
--- test/Betting.js
+++ test/Betting.js
@@ -1,13 +1,13 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
 const {
   loadFixture,
   setBalance,
-} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+} = require('@nomicfoundation/hardhat-toolbox/network-helpers');
 
-describe("Betting contract", () => {
+describe('Betting contract', () => {
   const deployBettingFixture = async () => {
-    const contract = await ethers.deployContract("Betting");
+    const contract = await ethers.deployContract('Betting');
     const [owner, address1, address2, address3, address4, address5] =
       await ethers.getSigners();
 
@@ -24,34 +24,39 @@ describe("Betting contract", () => {
     };
   };
 
-  describe("Deployment", () => {
-    it("Should set the right owner", async () => {
+  /** Hardhat tutorial */
+  describe('Deployment', () => {
+    it('Should set the right owner', async () => {
       const { owner, contract } = await loadFixture(deployBettingFixture);
 
       expect(await contract.owner()).to.equal(owner.address);
     });
   });
 
-  describe("Challanges", () => {
+  describe('Challanges', () => {
     /** challange [
      *  0 = challenged
      *  1 = challenger
      *  2 = judge
      *  3 = betAmount
-     *  4 = active
-     *  5 = accepted
-     *  6 = winner
+     *  4 = betBalance
+     *  5 = active
+     *  6 = accepted
+     *  7 = winner
+     *  8 = createdAt
+     *  9 = acceotedAt
+     *  10 = finishedAt
      * ]
      */
     const createBetFixture = async () => {
-      const { contract, owner, address1, address2, address3 } =
+      const { contract, owner, address1, address2, address3, address4 } =
         await loadFixture(deployBettingFixture);
 
-      await setBalance(address1.address, ethers.parseEther("100000.0"));
+      await setBalance(address1.address, ethers.parseEther('100000.0'));
 
       await contract
         .connect(address1)
-        .createChallange({ value: ethers.parseEther("2.0") });
+        .createChallange({ value: ethers.parseEther('2.0') });
 
       return {
         contract,
@@ -59,17 +64,18 @@ describe("Betting contract", () => {
         address1,
         address2,
         address3,
+        address4,
       };
     };
 
     const acceptChallengeFixture = async () => {
-      const { contract, owner, address1, address2, address3 } = await loadFixture(createBetFixture);
+      const { contract, owner, address1, address2, address3, address4 } = await loadFixture(createBetFixture);
 
-      await setBalance(address2.address, ethers.parseEther("10000.0"));
+      await setBalance(address2.address, ethers.parseEther('10000.0'));
 
       await contract
         .connect(address2)
-        .acceptChallenge(0, { value: ethers.parseEther("2.0") });
+        .acceptChallenge(0, { value: ethers.parseEther('2.0') });
 
       return {
         contract,
@@ -77,11 +83,12 @@ describe("Betting contract", () => {
         address1,
         address2,
         address3,
+        address4,
       }
     };
 
     const setJudgeFixture = async () => {
-      const { contract, owner, address1, address2, address3 } = await loadFixture(acceptChallengeFixture);
+      const { contract, owner, address1, address2, address3, address4 } = await loadFixture(acceptChallengeFixture);
 
       await contract
         .connect(address3)
@@ -93,37 +100,38 @@ describe("Betting contract", () => {
         address1,
         address2,
         address3,
+        address4,
       }
     };
 
-    it("Should create a new challange", async () => {
+    it('Should create a new challange', async () => {
       const { contract, address1 } = await loadFixture(deployBettingFixture);
 
-      await setBalance(address1.address, ethers.parseEther("100000.0"));
+      await setBalance(address1.address, ethers.parseEther('100000.0'));
 
       expect(
         await contract
           .connect(address1)
-          .createChallange({ value: ethers.parseEther("2.0") })
+          .createChallange({ value: ethers.parseEther('2.0') })
       )
-        .to.emit(contract, "ChallengeCreated")
-        .withArgs(address1.address, ethers.parseEther("2.0"));
+        .to.emit(contract, 'ChallengeCreated')
+        .withArgs(address1.address, ethers.parseEther('2.0'));
 
       const challange = await contract.bets(0);
 
       expect(challange[0]).to.equal(address1.address);
-      expect(challange[3]).to.equal(ethers.parseEther("2.0"));
+      expect(challange[3]).to.equal(ethers.parseEther('2.0'));
     });
 
-    it("Should accept challanges", async () => {
+    it('Should accept challanges', async () => {
       const { contract, address2 } = await loadFixture(createBetFixture);
 
-      await setBalance(address2.address, ethers.parseEther("100000.0"));
+      await setBalance(address2.address, ethers.parseEther('100000.0'));
 
       expect(await contract.connect(address2)
-        .acceptChallenge(0, { value: ethers.parseEther("2.0") })
+        .acceptChallenge(0, { value: ethers.parseEther('2.0') })
       )
-      .to.emit(contract, "ChallengeAccepted")
+      .to.emit(contract, 'ChallengeAccepted')
       .withArgs(address2.address, 0);
 
       const challange = await contract.bets(0);
@@ -131,46 +139,46 @@ describe("Betting contract", () => {
       expect(challange[1]).to.equal(address2.address)
     });
 
-    it("Should fail when ammout is different", async () => {
+    it('Should fail when ammout is different', async () => {
       const { contract, address2 } = await loadFixture(createBetFixture);
 
-      await setBalance(address2.address, ethers.parseEther("100000.0"));
+      await setBalance(address2.address, ethers.parseEther('100000.0'));
 
       await expect(contract.connect(address2)
-        .acceptChallenge(0, { value: ethers.parseEther("2.1") })
+        .acceptChallenge(0, { value: ethers.parseEther('2.1') })
       ).to.be
-      .revertedWith("Valor enviado diferente do valor da aposta.");
+      .revertedWith('Valor enviado diferente do valor da aposta.');
     });
 
-    it("Should fail if the challenge is already accepted", async () => {
+    it('Should fail if the challenge is already accepted', async () => {
       const { contract, address2, address3 } = await loadFixture(createBetFixture);
 
-      await setBalance(address2.address, ethers.parseEther("100000.0"));
-      await setBalance(address3.address, ethers.parseEther("100000.0"));
+      await setBalance(address2.address, ethers.parseEther('100000.0'));
+      await setBalance(address3.address, ethers.parseEther('100000.0'));
 
-      await contract.connect(address2).acceptChallenge(0, { value: ethers.parseEther("2.0") });
+      await contract.connect(address2).acceptChallenge(0, { value: ethers.parseEther('2.0') });
 
       await expect(contract.connect(address3)
-        .acceptChallenge(0, { value: ethers.parseEther("2.0") })
+        .acceptChallenge(0, { value: ethers.parseEther('2.0') })
       ).to.be
-      .revertedWith("Desafio nao esta ativo ou ja foi aceito.");
+      .revertedWith('Desafio nao esta ativo ou ja foi aceito.');
     });
 
-    it("Should fail if the challenger is the challenged", async () => {
+    it('Should fail if the challenger is the challenged', async () => {
       const { contract, address1 } = await loadFixture(createBetFixture);
 
       await expect(contract.connect(address1)
-        .acceptChallenge(0, { value: ethers.parseEther("2.0") })
+        .acceptChallenge(0, { value: ethers.parseEther('2.0') })
       ).to.be
-      .revertedWith("Voce nao pode aceitar o proprio desafio.");
+      .revertedWith('Voce nao pode aceitar o proprio desafio.');
     });
 
-    it("Should set the judge successfully", async () => {
+    it('Should set the judge successfully', async () => {
       const { contract, address3 } = await loadFixture(acceptChallengeFixture);
 
       await expect(contract.connect(address3)
         .setJudge(0)
-      ).to.emit(contract, "JudgeDefined")
+      ).to.emit(contract, 'JudgeDefined')
       .withArgs(address3.address);
 
       const challenge = await contract.bets(0);
@@ -178,38 +186,93 @@ describe("Betting contract", () => {
       expect(challenge[2]).to.equal(address3.address);
     });
 
-    it("Should fails if judge is the challenged", async () => {
+    it('Should fails if judge is the challenged', async () => {
       const { contract, address1 } = await loadFixture(acceptChallengeFixture);
 
       const challenge = await contract.bets(0);
 
       await expect(contract.connect(address1)
         .setJudge(0)
-      ).to.be.rejectedWith("O desafiado nao pode ser o juiz.");
+      ).to.be.rejectedWith('O desafiado nao pode ser o juiz.');
 
       expect(challenge[2]).to.equal(ethers.ZeroAddress);
     });
 
-    it("Should fails if judge is the challenger", async () => {
+    it('Should fails if judge is the challenger', async () => {
       const { contract, address2 } = await loadFixture(acceptChallengeFixture);
 
       const challenge = await contract.bets(0);
 
       await expect(contract.connect(address2).setJudge(0))
-        .to.be.rejectedWith("O desafiante nao pode ser o juiz.");
+        .to.be.rejectedWith('O desafiante nao pode ser o juiz.');
 
       expect(challenge[2]).to.equal(ethers.ZeroAddress);
     });
 
-    it("Should fail if it already has a judge", async () => {
+    it('Should fail if it already has a judge', async () => {
       const { contract, address3 } = await loadFixture(setJudgeFixture);
 
       const challenge = await contract.bets(0);
       
       await expect(contract.connect(address3).setJudge(0))
-        .to.be.rejectedWith("Ja existe um juiz para esse desafio");
+        .to.be.rejectedWith('Ja existe um juiz para esse desafio.');
 
       expect(challenge[2]).not.equal(ethers.ZeroAddress);
+    });
+
+    it('Should set the winner', async () => { 
+      const { contract, owner, address1, address2, address3 } = await loadFixture(setJudgeFixture);
+
+      const reward = Number(ethers.parseEther('4.0')) * 0.8;
+
+      expect(await contract.connect(address3).setWinner(0, address1)).to.emit(contract, 'ChallengeWinner').withArgs(address1, reward);
+
+      const challange = await contract.bets(0);
+      expect(challange[7]).to.equal(address1.address);
+      expect(challange[5]).to.equal(false);
+      expect(challange[10]).to.not.equal(0);
+    });
+
+    it('Should fail if it\'s wrong judge', async () => {
+      const { contract, address1, address2 } = await loadFixture(setJudgeFixture);
+
+      await expect(contract.connect(address2).setWinner(0, address1))
+      .to.be.rejectedWith('Somente o juiz pode declarar o vencedor.');
+    });
+
+    it('Should fail if the winner is neither the challenger nor the challenged', async () => {
+      const { contract, address3, address4 } = await loadFixture(setJudgeFixture);
+
+      await expect(contract.connect(address3).setWinner(0, address4))
+      .to.be.rejectedWith('O vencedor deve ser o desafiado ou o desafiante.');
+      
+      await expect(contract.connect(address3).setWinner(0, address3))
+      .to.be.rejectedWith('O vencedor deve ser o desafiado ou o desafiante.');
+    });
+
+    it('Should fail if the challange is inactive or opened', async () => {
+      const { contract, address3, address1 } = await loadFixture(createBetFixture);
+
+      await contract.connect(address3).setJudge(0);
+
+      await expect(contract.connect(address3).setWinner(0, address1))
+      .to.be.rejectedWith('Desafio nao esta ativo ou nao foi aceito');
+    });
+
+    it('Should create 30 chanlenges', async () => {
+      const { contract, address1 } = await loadFixture(deployBettingFixture);
+
+      setBalance(address1.address, ethers.parseEther("100000000"));
+
+      contract.connect(address1);
+
+      for (let i = 0; i < 30; i++) {
+        await contract.createChallange({value: ethers.parseEther('1.0')});
+      }
+
+      const bets = await contract.getBetsResponse();
+
+      expect(Number(bets[1])).to.equal(30)
     });
   });
 });
